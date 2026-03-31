39a3c54c048a94ed12a097ffc0e1a432c4b628e4
asinkpon <122337996+asinkpon@users.noreply.github.com>
2026-03-30 20:44:38 -0400

    Modify DetailedAlertSink to use DetailedAlert
    Updated invoke method to accept DetailedAlert for enhanced logging.

--- frauddetection/src/main/java/spendreport/DetailedAlertSink.java
+++ frauddetection/src/main/java/spendreport/DetailedAlertSink.java
@@ -1,13 +1,19 @@
 package spendreport;
 
+import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@PublicEvolving
+@SuppressWarnings("unused")
 public class DetailedAlertSink implements SinkFunction<DetailedAlert> {
+
+    private static final long serialVersionUID = 1L;
 
     private static final Logger LOG = LoggerFactory.getLogger(DetailedAlertSink.class);
 
+    // Modified to accept DetailedAlert instead of Alert for richer logging information
     @Override
     public void invoke(DetailedAlert value, Context context) {
         LOG.info(value.toString());
